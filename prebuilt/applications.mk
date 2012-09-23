4d0f87672e863fa025ef8de7c3d826a7440a7eab
napodan <david.guenet@gmail.com>
2012-09-23 16:54:54 +0200

    Adding Superuser.apk and link2sd script

--- prebuilt/applications.mk
+++ prebuilt/applications.mk
@@ -1,4 +1,4 @@
 
 PRODUCT_COPY_FILES += \
-vendor/cm/prebuilt/common/apps/Superuser.apk:system/app/Superuser.apk \
-vendor/napodan/prebuilt//etc/11link2sd:system/etc/init.d/11link2sd
+vendor/napodan/prebuilt/apps/Superuser.apk:system/app/Superuser.apk \
+vendor/napodan/prebuilt/etc/11link2sd:system/etc/init.d/11link2sd
