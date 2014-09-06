eb68a696000c668f26fce83020e302247c9c4eba
napodan <david.guenet@gmail.com>
2014-09-06 21:45:14 +0200

    Update build command

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,11 +2,30 @@ add_lunch_combo oxygen_bravo-user
 
 lunch oxygen_bravo-user
 
+export TOP=$ANDROID_BUILD_TOP
 build()
 {
   cd $ANDROID_BUILD_TOP
   [[ -d out ]] && \rm -rf out
+
+  # Bionic update
   ./bionic/libc/kernel/tools/update_all.py
   ./bionic/libc/tools/gensyscalls.py
+
+  # Build
   time make -j8 oxygen
+
+  # Verification
+  OUTFILE=`echo $OUT/update-oxygen-2.3.7-Bravo-*-signed.zip`
+  if [[ -f "$OUTFILE" ]]
+  then
+      bDate=`date +%Y%m%d_%H%M%S`
+      REP=../Build/$bDate
+      mkdir -p $REP
+      unzip -l $OUTFILE > $REP/listFiles.txt
+      find out/target/product/bravo/root  -exec du -b {} \; | sort -k2 >> $REP/listFiles.txt
+      cp $OUTFILE $REP/
+      repo manifest -r -o $REP/build.xml
+  fi
 }
+
